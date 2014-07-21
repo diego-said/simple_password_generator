9e082d070a34ed7f663ff4add651a968f92902a7
Diego Said <diego@doublelogic.com.br>
2014-07-21 07:43:43 -0300

    new ui files

--- src/br/com/doublelogic/spg/ui/SimplePasswordGeneratorActivity.java
+++ src/br/com/doublelogic/spg/ui/SimplePasswordGeneratorActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import br.com.doublelogic.spg.R;
 import br.com.doublelogic.spg.bean.PasswordSettings;
 import br.com.doublelogic.spg.common.RegExDefaults;
@@ -15,13 +14,13 @@ import br.com.doublelogic.spg.common.RegExDefaults;
 public class SimplePasswordGeneratorActivity extends Activity {
 
 	private static final int REQUEST_RESULT_PASSWORDS = 1;
-	
+
 	private PasswordSettings passSettings;
-	
+
 	private EditText editTextLength;
 	private EditText editTextQuantity;
 	private EditText editTextRegEx;
-	
+
 	private CheckBox checkBoxLetters;
 	private CheckBox checkBoxNumbers;
 	private CheckBox checkBoxSpecialChar;
@@ -34,7 +33,7 @@ public class SimplePasswordGeneratorActivity extends Activity {
 		loadUIReferences();
 
 		passSettings = new PasswordSettings();
-		
+
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 	}
 
@@ -42,29 +41,29 @@ public class SimplePasswordGeneratorActivity extends Activity {
 		editTextLength = (EditText) findViewById(R.id.editTextLength);
 		editTextQuantity = (EditText) findViewById(R.id.editTextQuantity);
 		editTextRegEx = (EditText) findViewById(R.id.editTextRegEx);
-		
+
 		checkBoxLetters = (CheckBox) findViewById(R.id.checkBoxLetters);
 		checkBoxNumbers = (CheckBox) findViewById(R.id.checkBoxNumbers);
 		checkBoxSpecialChar = (CheckBox) findViewById(R.id.checkBoxSpecialChar);
 	}
 
 	public void checkBoxClickHandler(View view) {
-		if(checkBoxLetters.isChecked()) {
-			if(checkBoxNumbers.isChecked() && checkBoxSpecialChar.isChecked()) {
+		if (checkBoxLetters.isChecked()) {
+			if (checkBoxNumbers.isChecked() && checkBoxSpecialChar.isChecked()) {
 				editTextRegEx.setText(String.valueOf(RegExDefaults.DEFAULT_REG_EX));
-			} else if(checkBoxNumbers.isChecked()) {
+			} else if (checkBoxNumbers.isChecked()) {
 				editTextRegEx.setText(String.valueOf(RegExDefaults.LETTERS_NUMBERS_REG_EX));
-			} else if(checkBoxSpecialChar.isChecked()) {
+			} else if (checkBoxSpecialChar.isChecked()) {
 				editTextRegEx.setText(String.valueOf(RegExDefaults.LETTERS_SPECIAL_CHAR_REG_EX));
 			} else {
 				editTextRegEx.setText(String.valueOf(RegExDefaults.LETTERS_REG_EX));
 			}
 		} else {
-			if(checkBoxNumbers.isChecked() && checkBoxSpecialChar.isChecked()) {
+			if (checkBoxNumbers.isChecked() && checkBoxSpecialChar.isChecked()) {
 				editTextRegEx.setText(String.valueOf(RegExDefaults.NUMBERS_SPECIAL_CHAR_REG_EX));
-			} else if(checkBoxNumbers.isChecked()) {
+			} else if (checkBoxNumbers.isChecked()) {
 				editTextRegEx.setText(String.valueOf(RegExDefaults.NUMBERS_REG_EX));
-			} else if(checkBoxSpecialChar.isChecked()) {
+			} else if (checkBoxSpecialChar.isChecked()) {
 				editTextRegEx.setText(String.valueOf(RegExDefaults.SPECIAL_CHAR_REG_EX));
 			}
 		}
@@ -73,28 +72,28 @@ public class SimplePasswordGeneratorActivity extends Activity {
 	public void generateClickHandler(View view) {
 		switch (view.getId()) {
 		case R.id.buttonGenerate:
-			
+
 			String length = editTextLength.getText().toString();
 			if ((length != null) && (length.trim().length() > 0)) {
 				passSettings.setLength(Integer.parseInt(length));
-			} 
-			
+			}
+
 			String quantity = editTextQuantity.getText().toString();
 			if ((quantity != null) && (quantity.trim().length() > 0)) {
 				passSettings.setQuantity(Integer.parseInt(quantity));
 			}
-			
+
 			String regEx = editTextRegEx.getText().toString();
 			if ((regEx != null) && (regEx.trim().length() > 0)) {
 				passSettings.setRegEx(regEx);
 			}
-			
+
 			Intent requestResult = new Intent(this, ResultPasswordsActivity.class);
-            requestResult.putExtra(PasswordSettings.KEY, passSettings);
-            startActivityForResult(requestResult, REQUEST_RESULT_PASSWORDS);
-			
+			requestResult.putExtra(PasswordSettings.KEY, passSettings);
+			startActivityForResult(requestResult, REQUEST_RESULT_PASSWORDS);
+
 			break;
 		}
 	}
-	
+
 }
