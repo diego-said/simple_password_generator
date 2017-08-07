37143b06ff37093db3a438c9c0563fa5128ea10a
Diego Alves Said Simão <diego@doublelogic.com.br>
2017-08-07 18:18:57 -0300

    saving settings

---
+++ app/src/main/java/br/com/doublelogic/spg/common/Preferences.java
@@ -0,0 +1,9 @@
+package br.com.doublelogic.spg.common;
+
+public enum Preferences {
+
+    SETTINGS,
+    KEEP_LAST_CONFIG,
+    NO_REPEAT;
+
+}
--- app/src/main/java/br/com/doublelogic/spg/ui/SettingsFragment.java
+++ app/src/main/java/br/com/doublelogic/spg/ui/SettingsFragment.java
@@ -1,6 +1,8 @@
 package br.com.doublelogic.spg.ui;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +10,20 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 
 import br.com.doublelogic.spg.R;
+import br.com.doublelogic.spg.common.Preferences;
 
 public class SettingsFragment extends Fragment {
 
     private CheckBox checkBoxKeepSettings;
     private CheckBox checkBoxNoRepeat;
 
+    private SharedPreferences settings;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings, container, false);
+
+        settings = getActivity().getSharedPreferences(Preferences.SETTINGS.name(), Context.MODE_PRIVATE);
 
         loadUIReferences(view);
 
@@ -25,22 +32,29 @@ public class SettingsFragment extends Fragment {
 
     private void loadUIReferences(View view) {
         checkBoxKeepSettings = (CheckBox) view.findViewById(R.id.checkBoxKeepSettings);
+        checkBoxKeepSettings.setChecked(settings.getBoolean(Preferences.KEEP_LAST_CONFIG.name(), false));
         checkBoxKeepSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                checkBoxClickHandler(v);
+                savePreferences();
             }
         });
 
         checkBoxNoRepeat = (CheckBox) view.findViewById(R.id.checkBoxNoRepeat);
+        checkBoxNoRepeat.setChecked(settings.getBoolean(Preferences.NO_REPEAT.name(), false));
         checkBoxNoRepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                checkBoxClickHandler(v);
+                savePreferences();
             }
         });
     }
 
-    public void checkBoxClickHandler(View view) {
-
+    private void savePreferences() {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(Preferences.KEEP_LAST_CONFIG.name(), checkBoxKeepSettings.isChecked());
+        editor.putBoolean(Preferences.NO_REPEAT.name(), checkBoxNoRepeat.isChecked());
+        editor.commit();
     }
 
 }
